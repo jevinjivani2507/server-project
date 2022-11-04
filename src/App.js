31ceeb730cea49fe21c2bb5c5196cc0512f12243
Jevin Jivani <jivani.jevin25@gmail.com>
2022-11-05 00:13:24 +0530

    Backgroud SET

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underlinep">
-      Hello World!!
+    <div className="bg-black h-screen flex justify-center items-center pt-20 px-20 pb-10">
+      {/* <div className="w-[40rem] h-[40rem] outline-none fixed -top-40 -right-20 blur-3xl blob1 -rotate-45"></div>
+      <div className="w-[40rem] h-[40rem] outline-none fixed -bottom-40 -left-20 blur-3xl blob2 rotate-[135deg]"></div> */}
+      <div className="w-full h-full bg-[#121212] rounded-xl"></div>
     </div>
   );
 }
